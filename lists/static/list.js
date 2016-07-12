0ad018d0623d6a51e6df829d6b189fe8a16fcf4e
RARvolt <rarvolt@gmail.com>
2016-07-12 08:06:36 +0200

    Wrap JS code in ready() function.

--- lists/static/list.js
+++ lists/static/list.js
@@ -1,3 +1,6 @@
-$('input').on('keypress', function () {
-    $('.has-error').hide();
+$(document).ready(function () {
+    $('input').on('keypress', function () {
+        $('.has-error').hide();
+    });
 });
+
